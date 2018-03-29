ed31e73c2554f759f800f04775e97959e2b94a1e
AldarBis <aldarbiyusov@gmail.com>
2018-03-29 08:13:53 +0000

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::ChuckNorris.fact
+  content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
